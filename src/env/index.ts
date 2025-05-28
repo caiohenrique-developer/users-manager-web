a35e79eb8c62f9b0911f5fa9b80caf457e42fe09
caiohenrique-developer <caiohenrique.developer@gmail.com>
2025-05-28 18:53:38 -0300

    Refactor package.json scripts and update environment variable validation

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,8 @@
+import "dotenv/config";
+
 import z from "zod";
 
-const envSchema = z.object({
-	VITE_API_URL: z.string().url().default("http://localhost:4000"),
-});
+const envSchema = z.object({ VITE_API_URL: z.string().url() });
 
 const { data, success, error } = envSchema.safeParse(import.meta.env);
 
